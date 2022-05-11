be72276480d4996a70364bc53b5e5bd78fd4dc26
Marciamuli <mar-muli@hotmail.com>
2022-05-11 17:13:59 -0300

    Projeto criado

---
+++ backend/src/main/java/com/dvsuperior/dsmovie3/Dsmovie3Application.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dsmovie3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsmovie3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsmovie3Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dvsuperior/dsmovie3/Dsmovie3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dsmovie3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsmovie3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
