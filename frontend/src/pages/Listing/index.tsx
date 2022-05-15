1dbd020e55f561d1b3af0adf5bcb519d5d207481
Marciamuli <mar-muli@hotmail.com>
2022-05-15 20:03:19 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+
+    //FORMA ERRADA
+    
+    axios.get('http://localhost:8080/movie?size=12&page=0')
+         .then(response => {
+             console.log(response.data);
+         });
 
     return (
         <>
